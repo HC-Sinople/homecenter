483811f587452a08a4fdc8682faed5ba178b3a9f
HC-Sinople <fabrice.lallemand@hc-domotique.fr>
2022-07-26 23:13:25 +0200

    Correction bug

--- desktop/php/homecenter3.php
+++ desktop/php/homecenter3.php
@@ -15,11 +15,16 @@ if (!isConnect('admin')) {
   $isConnected = $fibaroServ->isConnected();
   sendVarToJS('isConnected', $isConnected ); 
   
-  $isBlocked = $fibaroServ->isBlocked();
-  sendVarToJS('isBlocked', $isBlocked );   
+  if (!$isConnected {
+    throw new Exception('{{402 - Problème de connexion à la box Fibaro }}');
+  }else{
+    $isBlocked = $fibaroServ->isBlocked();
+    sendVarToJS('isBlocked', $isBlocked );   
+    
+    echo '<script>alert("TEST")</script>';
 
-
-  $fibaroServ->getDevicesList( true );
+    $fibaroServ->getDevicesList( true );
+  }
 
 ?>
 
